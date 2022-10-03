d40819baa98e220d8484f9acb3f7dc54d5672801
elysdominguez <elysdominguez2@gmail.com>
2022-10-03 13:16:36 +0200

    Create the models and the migrations for cities-neig-poi

---
+++ models/pointsofinterest.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class pointsOfInterest extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  pointsOfInterest.init(
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      lat: { type: DataTypes.FLOAT, allowNull: false },
+      lon: { type: DataTypes.FLOAT, allowNull: false },
+      description: { type: DataTypes.STRING, allowNull: false },
+      imageUrl: { type: DataTypes.STRING, allowNull: false },
+    },
+    {
+      sequelize,
+      modelName: "pointsOfInterest",
+    }
+  );
+  return pointsOfInterest;
+};
